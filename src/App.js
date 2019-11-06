44b7eaf2d103330446f0e4ef833c15cc5bf03e20
filippo mussoni <muxxo95@gmail.com>
2019-11-06 11:53:09 +0100

    per tutti i boss

--- src/App.js
+++ src/App.js
@@ -6,45 +6,77 @@ class App extends React.Component{
   constructor(props){
    super(props);
    this.state ={
-     name:'',
-    names:[]
-  }
+     fields:{
+       name:'',
+     surname:'',
+    
+  },
+  names:[]
+                }
   this.handlesubmit = this.handlesubmit.bind(this);
+  //this.handlesubmit = this.onInputChange.bind(this);
+  //this.onSurnameChange = this.onSurnameChange.bind(this);
+  //meglio fare senza arrow function 
   }
-  onNameChange =(evt) =>{
-    this.setState({name:evt.target.value});
-  }
+  //onNameChange =(evt) =>{
+  //  this.setState({name:evt.target.value});
+  //}
+  //onSurnameChange =(evt) =>{
+  //  this.setState({surname:evt.target.value});
+  //}
 
+  onInputChange =(evt) =>{
+    const fields = this.state.fields;
+    fields[evt.target.name] = evt.target.value;
+    this.setState({fields:fields});
+  }
 
   handlesubmit(event){
     event.preventDefault();
     //leggere
-    var name = this.state.name;
-    //console.log("hai scritto" + name);
+   
+    //console.log("hai scritto" + this.state.fields.name + this.state.fields.surname);
     //ora che ho il dato posso aggiornato lo stato 
     //let names = this.state.names.slice();
     //names.push(name);
-    let names = [...this.state.names, name];
+    let names = [...this.state.names, this.state.fields ];
     this.setState({names:names});
     //pulisco vecchio metodo averere ref nella form 
     //this.refs.name.value="";
     //pulisco
-    this.setState({name:''});
+    //this.setState({name:''});
+    //this.setState({surname:''});
+    const emptyfield = {...this.state.fields};
+    for(var key in Object.keys(emptyfield)){
+      if (emptyfield.hasOwnProperty(key)) {
+        emptyfield[key]=''
+      }
+    }
+  //object.keys(emptyfield).forEach(i => emptyfield[i]='');
+    
+  //this.setState({fields:emptyfield})
   }
 
   render(){
-    let listanomi = this.state.names.map((name, index) =>{
+    let listanomi = this.state.names.map((item, index) =>{
       return (
-        <li key={index}>{name}</li>
+        <li key={index}>{item.surname},{item.name}</li>
       );
     });
     return(
     <div>
       <h1>esercicccccio</h1>
       <form onSubmit={this.handlesubmit}>
-        <input type="text" placeholder="il tuo nome" 
-        value={this.state.name}
-        onChange={this.onNameChange}/>
+        <input type="text" 
+              placeholder="il tuo nome" 
+              name='name'
+              value={this.state.fields.name}
+              onChange={this.onInputChange}/>
+            <input type="text" 
+              placeholder="il tuo cognome" 
+              name='surname'
+              value={this.state.fields.surname}
+              onChange={this.onInputChange}/>
           <input type="submit" value="invia"/>
       </form>
       <div>
